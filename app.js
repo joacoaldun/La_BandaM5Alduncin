4eb027aad050c8920c19d83e7462ed3f57bb51a1
joacoaldun <joacoaldun@gmail.com>
2021-04-15 16:34:26 -0300

    MercadoLiebre5E1-Joaquin Alduncin

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const puerto = process.env.PORT
+const path = require('path');
+console.log('Joaquin probando')
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login')
+});
+        
+app.get('/registro', (req, res) => {
+     res.render('registro')
+    });
+
+     app.listen(3000, function() {
+    console.log("Servidor corriendo en el puerto 3000");
+})
